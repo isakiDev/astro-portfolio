f2b345b46f15ff3793dddbcf6a5cf9b29580ec0f
isakidev <gaspar.c.developer@gmail.com>
2025-10-14 23:40:37 -0300

    chore: init files

---
+++ src/data/experience.mock.ts
@@ -0,0 +1,50 @@
+import { type TimeLineData } from "@ui/timeline/time-line.interface";
+
+export const mockExperience: TimeLineData[] = [
+  {
+    title: "Data Governance Trainee",
+    subTitle: {
+      title: "Zurich Santander Chile",
+      url: "https://linkedin.com/company/zurich-santander-insurance-america",
+    },
+    date: "sep 2024 - sep 2025",
+    extraDetails: [
+      {
+        title: "Calidad de Datos",
+        description:
+          "Desarrollo de procesos de calidad de datos en Databricks, incluyendo la ingesta desde APIs para la obtención de tickets, creación de pipelines ETL, implementación de un motor de calidad con Great Expectations y construcción de dashboards para el monitoreo de los resultados.",
+        url: "https://www.linkedin.com/posts/zurich-santander-insurance-america_zurichsantanderabrchile-somoszs-jaejvenesprofesionales-activity-7369387772685598720-b-El",
+      },
+    ],
+  },
+  {
+    title: "Desarrollador FullStack",
+    subTitle: {
+      title: "No Country",
+      url: "https://www.linkedin.com/company/nocountrytalent",
+    },
+    date: "may 2024 - mar 2025",
+    extraDetails: [
+      {
+        title: "BarberHub",
+        description:
+          "Diseñé, probé y documenté la API de una aplicación web orientada a optimizar la reserva de citas en barberías. Trabajé en un equipo internacional, asegurando una solución robusta, escalable y bien documentada para garantizar una experiencia de usuario fluida.",
+        url: "https://www.linkedin.com/feed/update/urn:li:activity:7200603436743016449/",
+      },
+    ],
+  },
+  {
+    title: "Desarrollador FullStack",
+    subTitle: {
+      title: "ColTec Spa",
+      url: "https://www.linkedin.com/company/coltec-spa/",
+    },
+    date: "ene 2023 - jul 2023",
+    extraDetails: [
+      {
+        description:
+          "Participé en el desarrollo de una aplicación de gestión para salas de venta, restaurantes y sistemas de autoatención. Mi trabajo incluyó la creación de nuevos módulos desarrollados con PHP para la aplicación, como la gestión de impresoras, vacaciones y permisos de empleados. "
+      },
+    ],
+  },
+];
---
+++ src/data/socials.mock.ts
@@ -0,0 +1,21 @@
+import GitHub from "@icons/GitHub.astro";
+import LinkedIn from "@icons/LinkedIn.astro";
+import TikTok from "@icons/TikTok.astro";
+
+export const mockSocials = [
+  {
+    name: 'GitHub',
+    icon: GitHub,
+    href: "https://github.com/isakiDev",
+  },
+  {
+    name: 'LinkedIn',
+    icon: LinkedIn,
+    href: "https://www.linkedin.com/in/isakidev/",
+  },
+  {
+    name: 'TikTok',
+    icon: TikTok,
+    href: "https://www.tiktok.com/@isakidev",
+  },
+];
