ba05c7ada45f2e15b1cefae0e863a105d084995b
isakidev <gaspar.c.developer@gmail.com>
2026-01-02 18:14:50 -0300

    chore: :wrench: remove unused params

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,18 +3,11 @@ import { defineCollection, z } from "astro:content";
 const blog = defineCollection({
   schema: z.object({
     title: z.string(),
-    author: z.string(),
-    date: z.string(),
-    img: z.string().url(),
-    readtime: z.number(),
     description: z.string(),
-    buy: z.object({
-      spain: z.string().url(),
-      usa: z.string().url()
-    })
-  })
+    date: z.coerce.date(),
+    tags: z.array(z.string()).optional(),
+    draft: z.boolean().optional(),
+  }),
 });
 
 export const collections = { blog }
-
-// 26:18
