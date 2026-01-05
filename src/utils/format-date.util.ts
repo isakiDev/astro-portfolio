8efbbe68125ea3f9bb5d05cdacc69a24e0f5bd37
isakidev <gaspar.c.developer@gmail.com>
2026-01-05 14:22:28 -0300

    refactor: :recycle: components

--- src/utils/format-date.util.ts
+++ src/utils/format-date.util.ts
@@ -1,7 +1,7 @@
 export const formatDate = (date: Date) => {
   return Intl.DateTimeFormat('es-CL', {
     year: 'numeric',
-    month: 'long',
+    month: 'short',
     day: 'numeric',
   }).format(date)
 }
