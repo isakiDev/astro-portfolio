b59780d37c96bf591cc09a77124574af7631d01e
isakidev <gaspar.c.developer@gmail.com>
2026-01-05 23:25:56 -0300

    refactor: :recycle: move and rename files

--- src/interfaces/time-line.interface.ts
+++ src/interfaces/time-line.interface.ts
@@ -1,12 +1,12 @@
 export interface Detail {
   title?: string;
-  description?: string | string[];
+  description: string | string[];
   url?: string;
 }
 
 export interface TimeLineData {
-  title?: string;
-  subTitle?: { title: string, url?: string }
-  date?: string;
+  title: string;
+  subTitle: { title: string, url?: string }
+  date: string;
   extraDetails?: Detail[];
 }
